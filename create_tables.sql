d04494e55e9dddce8051c0071319b9fa3d77f135
Darina <darina.eva00.00@mail.ru>
2025-06-28 21:44:53 +0300

    fix table creation

--- create_tables.sql
+++ create_tables.sql
@@ -1,47 +1,47 @@
-CREATE TABLE genre IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS genre (
     id SERIAL PRIMARY KEY,
     name VARCHAR(80) NOT NULL
 );
 
-CREATE TABLE artist IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS artist (
     id SERIAL PRIMARY KEY,
     name VARCHAR(150) NOT NULL
 );
 
-CREATE TABLE artist_genre IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS artist_genre (
     id SERIAL PRIMARY KEY,
     genre_id INTEGER REFERENCES genre(id),
     artist_id INTEGER REFERENCES artist(id),
     UNIQUE(genre_id, artist_id)
 );
 
-CREATE TABLE album IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS album (
     id SERIAL PRIMARY KEY,
     name VARCHAR(150) NOT NULL,
     year DATE NOT NULL
 );
 
-CREATE TABLE artist_album IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS artist_album (
     id SERIAL PRIMARY KEY,
     artist_id INTEGER REFERENCES artist(id),
     album_id INTEGER REFERENCES album(id),
     UNIQUE(artist_id, album_id)
 );
 
-CREATE TABLE song IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS song (
     id SERIAL PRIMARY KEY,
     name VARCHAR(120) NOT NULL,
     duration INTEGER NOT NULL,
     album_id INTEGER REFERENCES album(id)
 );
 
-CREATE TABLE collection IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS collection (
     id SERIAL PRIMARY KEY,
     name VARCHAR(120) NOT NULL,
     year DATE NOT NULL
 );
 
-CREATE TABLE collection_song IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS collection_song (
     id SERIAL PRIMARY KEY,
     collection_id INTEGER REFERENCES collection(id),
     song_id INTEGER REFERENCES song(id),
